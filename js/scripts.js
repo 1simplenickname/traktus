46f4c93eb4f03edf01ac767e41fea01cff47d9db
Niks Ļeoņenko <niks.leonenko@icloud.com>
2018-11-06 15:28:53 +0200

    Fixed CORS error

--- js/scripts.js
+++ js/scripts.js
@@ -21,16 +21,55 @@ function showStats() {
     let apiKey = "46ebcab57a63082b8025c12ad813efeadb8facb525aef41d6f50a925721822f8";
     let requestURL = "https://api.trakt.tv/users/" + user + "?extended=full";
 
-    $.ajax({
-        headers: {
-            'Content-Type': 'application/json',
-            'trakt-api-version': '2',
-            'trakt-api-key': apiKey
-        },
-        url: requestURL,
-        function(data) {
-            console.log(data);
+    // $.ajax({
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         'trakt-api-version': '2',
+    //         'trakt-api-key': apiKey
+    //     },
+    //     url: requestURL,
+    //     function(data) {
+    //         console.log(data);
+    //     }
+    // });
+
+    if(XMLHttpRequest)
+    {
+        let request = new XMLHttpRequest();
+        if("withCredentials" in request) {
+            request.open('GET', requestURL, true);
+            request.setRequestHeader('Content-Type', 'application/json');
+            request.setRequestHeader('trakt-api-version', '2');
+            request.setRequestHeader('trakt-api-key', apiKey);
+            request.onreadystatechange = function () {
+                if (this.readyState === 4) {
+                    console.log('Status:', this.status);
+                    console.log('Headers:', this.getAllResponseHeaders());
+                    console.log('Body:', this.responseText);
+                }
+            };
+            request.send();
         }
-    });
+
+    }
+
+    // let request = new XMLHttpRequest();
+    //
+    // request.open('GET', requestURL);
+    //
+    // request.setRequestHeader('Content-Type', 'application/json');
+    // request.setRequestHeader('trakt-api-version', '2');
+    // request.setRequestHeader('trakt-api-key', apiKey);
+    //
+    // request.onreadystatechange = function () {
+    //     if (this.readyState === 4) {
+    //         console.log('Status:', this.status);
+    //         console.log('Headers:', this.getAllResponseHeaders());
+    //         console.log('Body:', this.responseText);
+    //     }
+    // };
+    //
+    // request.send();
+
 
 }
