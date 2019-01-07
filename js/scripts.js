c004e06437e06930f8d60d235133791bac5c9a21
Niks Ļeoņenko <niks.leonenko@icloud.com>
2019-01-07 20:47:57 +0200

    Added user avatar support

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,13 @@ let home = document.getElementById("home");
 let stats = document.getElementById("stats");
 let movies = document.getElementById("movies");
 let episodes = document.getElementById("episodes");
-let about = document.getElementById("about");
 
 let searchBar = document.getElementById("searchBar");
 let usernameField = document.getElementById("username");
 let joined = document.getElementById("joined");
 
-let requestURL, userData, userStats = "";
-
 let apiKey = "46ebcab57a63082b8025c12ad813efeadb8facb525aef41d6f50a925721822f8";
+let requestURL, fullUserData, userStats, userAvatar = "";
 
 function toggleVisibility(target, state) {
 
@@ -29,21 +27,16 @@ function newRequest(url, arg) {
         request.onreadystatechange = function () {
             if (this.readyState === 4) {
 
-                console.log('Status:', this.status);
-                console.log('Headers:', this.getAllResponseHeaders());
+                if (arg === "fullUserData") {
 
-                if (arg === "userData") {
-
-                    userData = this.responseText;
-                    userData = JSON.parse(userData);
-
+                    fullUserData = this.responseText;
+                    fullUserData = JSON.parse(fullUserData);
                     loadUserStats();
 
                 } else if (arg === "userStats") {
 
                     userStats = this.responseText;
                     userStats = JSON.parse(userStats);
-
                     showUserStats();
 
                 }
@@ -60,31 +53,44 @@ function newRequest(url, arg) {
 
 function loadUserData() {
 
-    let user = searchBar.value;
-    usernameField.innerHTML = user;
-
     home.classList.add("fadeOut");
     toggleVisibility("home","hidden");
     stats.classList.add("fadeIn");
     toggleVisibility("stats","visible");
 
+    let user = searchBar.value;
+    usernameField.innerHTML = user;
+
+    let joined = document.getElementById("joined");
+    joined.innerHTML = "Loading " + user + "'s stats";
+
     requestURL = "https://cors-anywhere.herokuapp.com/" + "https://api.trakt.tv/users/" + user + "?extended=full";
-    newRequest(requestURL, "userData");
+    newRequest(requestURL, "fullUserData");
 
 }
 
 function loadUserStats() {
 
     let user = searchBar.value;
+    let avatar = document.getElementById("avatar");
+    let avatarPlaceholder = document.getElementById("avatarPlaceholder");
+
+    avatarPlaceholder.classList.add("fadeOut");
+    avatar.classList.add("fadeIn");
+
+    toggleVisibility("avatar", "visible");
+    toggleVisibility("avatarPlaceholder", "hidden");
+    userAvatar = fullUserData.images.avatar.full;
+    avatar.style.backgroundImage = "url(" + userAvatar + ")";
 
     joined.classList.add("fadeIn");
-    let joinedEdited = new Date(userData.joined_at).toString().substr(4).substring(0,11);
+    let joinedEdited = new Date(fullUserData.joined_at).toString().substr(4).substring(0,11);
     joined.innerHTML = "Trakt user since " + joinedEdited.substring(6, 0) + "," + joinedEdited.substr(6);
 
     requestURL = "https://cors-anywhere.herokuapp.com/" + "https://api.trakt.tv/users/" + user + "/stats";
     newRequest(requestURL, "userStats");
 
-    console.log(userStats);
+    return userAvatar;
 
 }
 
@@ -102,13 +108,19 @@ function showUserStats() {
     let moviesCalculated = document.getElementById("moviesCalculated");
     let episodesCalculated = document.getElementById("episodesCalculated");
 
-    moviesWatched.innerHTML = userStats.movies.watched;
-    moviesPlayed.innerHTML = userStats.movies.plays;
-    moviesCalculated.innerHTML = Math.round(userStats.movies.minutes / 60);
+    moviesWatched.innerHTML = userStats.movies.watched + " movies";
+    moviesPlayed.innerHTML = userStats.movies.plays + " movies";
+    moviesCalculated.innerHTML = Math.round(userStats.movies.minutes / 60) + " hours";
 
-    episodesWatched.innerHTML = userStats.episodes.watched;
-    episodesPlayed.innerHTML = userStats.episodes.plays;
-    episodesCalculated.innerHTML = Math.round(userStats.episodes.minutes / 60);
+    episodesWatched.innerHTML = userStats.episodes.watched + " episodes";
+    episodesPlayed.innerHTML = userStats.episodes.plays + " episodes";
+    episodesCalculated.innerHTML = Math.round(userStats.episodes.minutes / 60) + " hours";
+
+}
+
+function openUserAvatar() {
+
+    window.open(userAvatar);
 
 }
 
@@ -116,5 +128,11 @@ function openUserOnTrakt() {
 
     let user = searchBar.value;
     window.open("https://www.trakt.tv/users/" + user);
+
+}
+
+function poweredByTrakt() {
+
+    window.open("https://trakt.tv");
 
 }
